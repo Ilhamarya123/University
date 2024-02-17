6c4e7153cb3a61d51b2df7cdb247a727224084b4
unknown <ilhamarya319@gmail.com>
2024-02-17 15:19:04 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './Components/Navbar';
+import Hero from './Components/Hero';
+import Card from './Components/Card';
+import Bootcamp from './Components/Bootcamp';
+import Course from './Components/Course';
+import Workshop from './Components/Workshop';
+import Logo from './Components/Logo';
+import Banner from './Components/Banner';
+import Footer from './Components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-dark'>
+    <Navbar/>
+    <Hero/>
+    <Card/>
+    <Bootcamp/>
+    <Course/>
+    <Workshop/>
+    <Logo/>
+   <Banner/>
+   <Footer/>
     </div>
   );
 }
